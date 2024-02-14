cfac6a055fa8ea931c97dbe5711301d3251bd8b7
ReDxDaGer <yashmehrotra45@gmail.com>
2024-02-14 15:22:37 +0530

    Updated the error handling

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def translate():
     else:
         return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', error_code=404, error_message="Page Not Found"), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('error.html', error_code=500, error_message="Internal Server Error"), 500
+
 if __name__ == "__main__":
     app.run(debug=True)
 
