3dfeeb6252b2c6bd7f63bb81b03ddeaf3fc02f22
ReDxDaGer <yashmehrotra45@gmail.com>
2024-02-26 16:27:30 +0530

    Updated the port code

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ def internal_server_error(error):
     return render_template('error.html', error_code=500, error_message="Internal Server Error"), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0" , port=5000, debug=True)
